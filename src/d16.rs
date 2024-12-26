e8ffa40d188fa4e543830e2aee22b1d7cbcf26e1
cptkidd62 <zelinka15@gmail.com>
2024-12-26 15:11:36 +0100

    day 16 part 1 success

--- src/d16.rs
+++ src/d16.rs
@@ -1,53 +1,11 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::HashMap, u128};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Dir {
     Up,
     Right,
     Down,
     Left,
-}
-
-#[derive(Debug, Clone)]
-struct State {
-    deer: (usize, usize),
-    visited: HashSet<(usize, usize)>,
-    score: usize,
-    direction: Dir,
-}
-
-impl State {
-    fn is_valid_move(self: &Self, n_loc: (usize, usize)) -> bool {
-        !self.visited.contains(&n_loc)
-    }
-    fn do_move(self: &Self, n_loc: (usize, usize)) -> Self {
-        let mut n_visited = self.visited.clone();
-        n_visited.insert(n_loc);
-        let mut score_inc = 1;
-        let mut n_dir = self.direction;
-        if n_loc.0 > self.deer.0 && (n_dir == Dir::Left || n_dir == Dir::Right) {
-            n_dir = Dir::Down;
-            score_inc += 1000;
-        }
-        if n_loc.0 < self.deer.0 && (n_dir == Dir::Left || n_dir == Dir::Right) {
-            n_dir = Dir::Up;
-            score_inc += 1000;
-        }
-        if n_loc.1 > self.deer.1 && (n_dir == Dir::Up || n_dir == Dir::Down) {
-            n_dir = Dir::Right;
-            score_inc += 1000;
-        }
-        if n_loc.1 < self.deer.1 && (n_dir == Dir::Up || n_dir == Dir::Down) {
-            n_dir = Dir::Left;
-            score_inc += 1000;
-        }
-        Self {
-            deer: n_loc,
-            visited: n_visited,
-            score: self.score + score_inc,
-            direction: n_dir,
-        }
-    }
 }
 
 #[derive(Debug, Clone)]
@@ -57,41 +15,239 @@ struct Maze {
     end: (usize, usize),
 }
 
-impl Maze {
-    fn is_in_end(self: &Self, deer: (usize, usize)) -> bool {
-        deer == self.end
+fn calculate_trans_costs(
+    start_v: &(usize, usize),
+    end_v: &(usize, usize),
+    map: &HashMap<Dir, u128>,
+) -> HashMap<Dir, u128> {
+    use Dir::*;
+    let mut nmap = HashMap::new();
+    let &up_c = map.get(&Up).unwrap();
+    let &right_c = map.get(&Right).unwrap();
+    let &down_c = map.get(&Down).unwrap();
+    let &left_c = map.get(&Left).unwrap();
+    // up dir
+    if start_v.0 < end_v.0 {
+        // down
+        nmap.insert(
+            Up,
+            (up_c + 2000 + 1 + 2000)
+                .min(right_c + 1000 + 1 + 2000)
+                .min(down_c + 1 + 2000)
+                .min(left_c + 1000 + 1 + 2000),
+        );
+    } else if start_v.0 > end_v.0 {
+        // up
+        nmap.insert(
+            Up,
+            (up_c + 1)
+                .min(right_c + 1000 + 1)
+                .min(down_c + 2000 + 1)
+                .min(left_c + 1000 + 1),
+        );
+    } else if start_v.1 < end_v.1 {
+        // right
+        nmap.insert(
+            Up,
+            (up_c + 1000 + 1 + 1000)
+                .min(right_c + 1 + 1000)
+                .min(down_c + 1000 + 1 + 1000)
+                .min(left_c + 2000 + 1 + 1000),
+        );
+    } else if start_v.1 > end_v.1 {
+        // left
+        nmap.insert(
+            Up,
+            (up_c + 1000 + 1 + 1000)
+                .min(right_c + 2000 + 1 + 1000)
+                .min(down_c + 1000 + 1 + 1000)
+                .min(left_c + 1 + 1000),
+        );
+    }
+    // right dir
+    if start_v.0 < end_v.0 {
+        // down
+        nmap.insert(
+            Right,
+            (up_c + 2000 + 1 + 1000)
+                .min(right_c + 1000 + 1 + 1000)
+                .min(down_c + 1 + 1000)
+                .min(left_c + 1000 + 1 + 1000),
+        );
+    } else if start_v.0 > end_v.0 {
+        // up
+        nmap.insert(
+            Right,
+            (up_c + 1 + 1000)
+                .min(right_c + 1000 + 1 + 1000)
+                .min(down_c + 2000 + 1 + 1000)
+                .min(left_c + 1000 + 1 + 1000),
+        );
+    } else if start_v.1 < end_v.1 {
+        // right
+        nmap.insert(
+            Right,
+            (up_c + 1000 + 1)
+                .min(right_c + 1)
+                .min(down_c + 1000 + 1)
+                .min(left_c + 2000 + 1),
+        );
+    } else if start_v.1 > end_v.1 {
+        // left
+        nmap.insert(
+            Right,
+            (up_c + 1000 + 1 + 2000)
+                .min(right_c + 2000 + 1 + 2000)
+                .min(down_c + 1000 + 1 + 2000)
+                .min(left_c + 1 + 2000),
+        );
+    }
+    // down dir
+    if start_v.0 < end_v.0 {
+        // down
+        nmap.insert(
+            Down,
+            (up_c + 2000 + 1)
+                .min(right_c + 1000 + 1)
+                .min(down_c + 1)
+                .min(left_c + 1000 + 1),
+        );
+    } else if start_v.0 > end_v.0 {
+        // up
+        nmap.insert(
+            Down,
+            (up_c + 1 + 2000)
+                .min(right_c + 1000 + 1 + 2000)
+                .min(down_c + 2000 + 1 + 2000)
+                .min(left_c + 1000 + 1 + 2000),
+        );
+    } else if start_v.1 < end_v.1 {
+        // right
+        nmap.insert(
+            Down,
+            (up_c + 1000 + 1 + 1000)
+                .min(right_c + 1 + 1000)
+                .min(down_c + 1000 + 1 + 1000)
+                .min(left_c + 2000 + 1 + 1000),
+        );
+    } else if start_v.1 > end_v.1 {
+        // left
+        nmap.insert(
+            Down,
+            (up_c + 1000 + 1 + 1000)
+                .min(right_c + 2000 + 1 + 1000)
+                .min(down_c + 1000 + 1 + 1000)
+                .min(left_c + 1 + 1000),
+        );
+    }
+    // left dir
+    if start_v.0 < end_v.0 {
+        // down
+        nmap.insert(
+            Left,
+            (up_c + 2000 + 1 + 1000)
+                .min(right_c + 1000 + 1 + 1000)
+                .min(down_c + 1 + 1000)
+                .min(left_c + 1000 + 1 + 1000),
+        );
+    } else if start_v.0 > end_v.0 {
+        // up
+        nmap.insert(
+            Left,
+            (up_c + 1 + 1000)
+                .min(right_c + 1000 + 1 + 1000)
+                .min(down_c + 2000 + 1 + 1000)
+                .min(left_c + 1000 + 1 + 1000),
+        );
+    } else if start_v.1 < end_v.1 {
+        // right
+        nmap.insert(
+            Left,
+            (up_c + 1000 + 1 + 2000)
+                .min(right_c + 1 + 2000)
+                .min(down_c + 1000 + 1 + 2000)
+                .min(left_c + 2000 + 1 + 2000),
+        );
+    } else if start_v.1 > end_v.1 {
+        // left
+        nmap.insert(
+            Left,
+            (up_c + 1000 + 1)
+                .min(right_c + 2000 + 1)
+                .min(down_c + 1000 + 1)
+                .min(left_c + 1),
+        );
     }
 
-    fn find_min(self: &Self, state: State) -> usize {
-        if self.is_in_end(state.deer) {
-            state.score
-        } else {
-            let transitions = self.transitions.get(&state.deer).unwrap().clone();
-            let possible_moves = transitions
-                .iter()
-                .filter(|&m| state.is_valid_move(*m))
-                .collect::<Vec<_>>();
-            if possible_moves.len() == 0 {
-                usize::MAX
+    nmap
+}
+
+impl Maze {
+    fn generate_all_costs(self: &Self) -> HashMap<(usize, usize), HashMap<Dir, u128>> {
+        use Dir::*;
+        let mut map = HashMap::new();
+        let mut q = HashMap::new();
+        for &k in self.transitions.keys() {
+            let mut hmap = HashMap::new();
+            if k == self.start {
+                hmap.insert(Up, 1000);
+                hmap.insert(Right, 0);
+                hmap.insert(Down, 1000);
+                hmap.insert(Left, 1000);
+                q.insert(k, 0);
             } else {
-                possible_moves
-                    .iter()
-                    .map(|&m| self.find_min(state.do_move(*m)))
-                    .min()
-                    .unwrap()
+                hmap.insert(Up, u128::MAX - 3000);
+                hmap.insert(Right, u128::MAX - 3000);
+                hmap.insert(Down, u128::MAX - 3000);
+                hmap.insert(Left, u128::MAX - 3000);
+                q.insert(k, u128::MAX - 3000);
+            }
+            map.insert(k, hmap);
+        }
+        while q.len() > 0 {
+            let u = q
+                .iter()
+                .fold(
+                    ((0, 0), u128::MAX),
+                    |(av, ae), (&v, &e)| {
+                        if e < ae {
+                            (v, e)
+                        } else {
+                            (av, ae)
+                        }
+                    },
+                )
+                .0;
+            q.remove(&u);
+
+            for v in self.transitions.get(&u).unwrap() {
+                if q.contains_key(v) {
+                    let alt = calculate_trans_costs(&u, v, map.get(&u).unwrap());
+                    let ent = map.entry(*v).or_default();
+                    let mut nc = u128::MAX;
+                    let up_ent = ent.entry(Up).or_default();
+                    *up_ent = up_ent.clone().min(*alt.get(&Up).unwrap());
+                    nc = nc.min(*up_ent);
+                    let right_ent = ent.entry(Right).or_default();
+                    *right_ent = right_ent.clone().min(*alt.get(&Right).unwrap());
+                    nc = nc.min(*right_ent);
+                    let down_ent = ent.entry(Down).or_default();
+                    *down_ent = down_ent.clone().min(*alt.get(&Down).unwrap());
+                    nc = nc.min(*down_ent);
+                    let left_ent = ent.entry(Left).or_default();
+                    *left_ent = left_ent.clone().min(*alt.get(&Left).unwrap());
+                    nc = nc.min(*left_ent);
+                    let q_ent = q.entry(*v).or_default();
+                    *q_ent = q_ent.clone().min(nc);
+                }
             }
         }
+        map
     }
 
-    fn generate_start_state(self: &Self) -> State {
-        let mut n_vis = HashSet::new();
-        n_vis.insert(self.start);
-        State {
-            deer: self.start,
-            visited: n_vis,
-            score: 0,
-            direction: Dir::Right,
-        }
+    fn min_cost_to_end(self: &Self) -> u128 {
+        let map = self.generate_all_costs();
+        *map.get(&self.end).unwrap().values().min().unwrap()
     }
 }
 
@@ -173,8 +329,7 @@ mod tests {
     fn task1() {
         println!("{}", {
             let maze = load_data();
-            // println!("{:?}", maze);
-            maze.find_min(maze.generate_start_state())
+            maze.min_cost_to_end()
         });
     }
 
